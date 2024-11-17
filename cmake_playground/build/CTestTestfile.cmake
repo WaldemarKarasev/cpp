c57d614e69f28726eeaa012fc48061ae994defc9
Vladimir <karasev.vladimir.2000@gmail.com>
2024-11-17 23:47:03 +0300

    Update

--- cmake_playground/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pingvinus/prog/cpp/cmake_playground
-# Build directory: /home/pingvinus/prog/cpp/cmake_playground/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cpp_lectures/cpp_playground/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-
-const char* Cstr = "constant string\0";
-
-int main()
-{
-	std::cout << "Hello" << std::endl;
-}
